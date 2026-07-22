d6284cea4416a4a0b84a34f63befb906809f4c49
mrcodeislife718 <mrcodeislife718@proton.me>
2026-07-22 12:33:40 -0400

    Prepare dynamic catalog schema before seeding

--- backend/scripts/seedUniversal.js
+++ backend/scripts/seedUniversal.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const sequelize = require('../config/database');
 const migrateLegacyUniversalSchema = require('../migrations/legacyUniversalMigration');
 const migrateCompleteFlowSchema = require('../migrations/completeFlowMigration');
+const migrateStoreCatalogSchema = require('../migrations/storeCatalogMigration');
 
 async function run() {
   await sequelize.authenticate();
   await migrateLegacyUniversalSchema();
   await migrateCompleteFlowSchema();
+  await migrateStoreCatalogSchema();
   require('../seed');
 }
 
